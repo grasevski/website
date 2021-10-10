f8d945116e04860696598bd1ade1a15cf00f58df
r007 <moninsergei@gmail.com>
2021-10-11 03:07:09 +0400

    Add Notification component, also display the notification message at the bottom of live page

---
+++ src/components/Notification/NotificationTextDetails.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1.5rem 0;
+`;
+
+const NotificationTitle = styled.p`
+  font-size: 1.4rem;
+  font-weight: 600;
+  line-height: 1.28572;
+  letter-spacing: 0.16px;
+  margin: 0 0.4rem 0 0;
+`;
+
+const NotificationSubtitle = styled.div`
+  font-size: 1.4rem;
+  font-weight: 400;
+  line-height: 1.28572;
+  letter-spacing: 0.16px;
+  word-break: break-word;
+`;
+
+const NotificationTextDetails = ({ title, subtitle, caption, children, ...other }) => (
+  <TextWrapper {...other}>
+    <NotificationTitle>{title}</NotificationTitle>
+    <NotificationSubtitle>{subtitle}</NotificationSubtitle>
+    {children}
+  </TextWrapper>
+);
+
+NotificationTextDetails.propTypes = {
+  /**
+   * Pass in the children that will be rendered in NotificationTextDetails
+   */
+  children: PropTypes.node,
+  /**
+   * Specify the title
+   */
+  title: PropTypes.string,
+  /**
+   * Specify the sub-title
+   */
+  subtitle: PropTypes.node,
+  /**
+   * Specify the caption
+   */
+  caption: PropTypes.node,
+};
+
+NotificationTextDetails.defaultProps = {
+  children: null,
+  title: 'title',
+  subtitle: 'subtitle',
+  caption: 'caption',
+};
+
+export default NotificationTextDetails;
