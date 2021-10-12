ad953a23d1491ea61c079ce46c07f5a780b7a51a
r007 <moninsergei@gmail.com>
2021-10-12 18:48:47 +0400

    Clean up the code of 2021agm page a little bit

--- src/pages/2021agm.js
+++ src/pages/2021agm.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
 import { Grid, Col, Row } from 'react-styled-flexboxgrid/src';
 import Layout from '../layouts/Layout';
 import SEO from '../components/SEO';
 import Heading from '../components/Heading';
 import { Spacing } from '../components/common';
 
-const AGMPage = ({ data }) => (
+const AGMPage = () => (
   <Layout>
     <SEO title="2021 Ocius AGM" description="AGM Landing Page" />
-    <Grid>
-      <Row>
+    <Grid className="centered">
+      <Row className="left-align">
         <Col xs={12} md={12} lg={12}>
           <Spacing $value="80px" />
           <Heading level={1} size="huge" weight="thick" underline="left">
@@ -20,18 +18,17 @@ const AGMPage = ({ data }) => (
         </Col>
       </Row>
 
-      <Heading level={2} size="medium">
-        <strong>Dear Shareholders,</strong>
+      <Heading level={2} size="medium" weight="thick">
+        Dear Shareholders,
       </Heading>
 
-      <div className="centered">
-        <Heading level={2}>
-          Instructions on how to attend our virtual AGM will be posted here on <b> Monday 29 Nov. </b>
-        </Heading>
-        <Heading level={2} weight="thick">
-          AGM 4.30pm Tuesday 30 November.
-        </Heading>
-      </div>
+      <Heading level={3} size="small" as="p" style={{ lineHeight: 1.6 }}>
+        Instructions on how to attend our virtual AGM will be posted here on{' '}
+        <strong> Monday 29 Nov.</strong>
+        <br />
+        <strong>AGM 4.30pm Tuesday 30 November.</strong>
+      </Heading>
+
       <Spacing $value="100px" />
     </Grid>
   </Layout>
