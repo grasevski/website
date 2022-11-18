eddc98a63255b266d37fd4044cc7b064cf6a46a0
Nicholas Grasevski <nicholas.grasevski@ocius.com.au>
2022-11-18 17:26:09 +1100

    fix(odometer): lazy import

--- src/components/Odometer.js
+++ src/components/Odometer.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import mq from '../common/mq';
-import Odometer from 'react-odometerjs';
+import loadable from '@loadable/component';
 import 'odometer/themes/odometer-theme-car.css';
 
 const Container = styled.div`
@@ -22,6 +22,7 @@ const Container = styled.div`
 `;
 
 function ReactOdometer() {
+  const Odometer = loadable(() => import('react-odometerjs'));
   return <Container><Odometer value={20520.7} format="(,ddd).d" />NM</Container>;
 }
 
